23cfb950a467a0e8cf87d7ae9a4f6018ac8fc7c2
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2016-12-01 20:22:42 +0100

    on vire waitConnection qui sert pas à grand chose

--- source/entities/index.js
+++ source/entities/index.js
@@ -43,29 +43,6 @@ class Entities extends EventEmitter {
     this.settings = settings;
     /** le pool de connexion */
     this.database = mysql.createPool(this.settings.database);
-    var pool = this.database
-    /**
-     * Tente d'obtenir une connexion du pool et recommence plus tard sinon
-     * (max 6 fois et 2.5s)
-     * C'est complémentaire avec le queueLimit du pool, si la file d'attente est pleine
-     * on attend un peu pour avoir une place dans la queue
-     * @param next
-     */
-    pool.waitConnection = function waitConnection (next) {
-      function nextTry () {
-        tries++
-        pool.getConnection(function (error, cnx) {
-          if (error) {
-            if (tries < 7) setTimeout(nextTry, 500)
-            else next(new Error('DB connection impossible after 6 tries (each 500ms)'))
-          } else {
-            next(null, cnx)
-          }
-        })
-      }
-      var tries = 0
-      nextTry()
-    }
   }
 
   /**
