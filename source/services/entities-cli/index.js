31f6b69ac80f7a0d152155bdb5be502e22f39dc9
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2018-08-01 20:44:43 +0200

    add validAll command

--- source/services/entities-cli/index.js
+++ source/services/entities-cli/index.js
@@ -4,6 +4,7 @@ const count = require('./count')
 const purgeDeleted = require('./purgeDeleted')
 const reindexAll = require('./reindexAll')
 const select = require('./select')
+const validAll = require('./validAll')
 
 /**
  * Service de gestion des entités via cli
@@ -15,7 +16,8 @@ module.exports = function () {
       count,
       purgeDeleted,
       reindexAll,
-      select
+      select,
+      validAll
     })
   }
 }
