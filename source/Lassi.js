eae9e3ad0e6f8ff16bfa5adcf6a93edfd817ed1b
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2019-07-18 18:32:41 +0200

    modif de forme

--- source/Lassi.js
+++ source/Lassi.js
@@ -110,62 +110,59 @@ class Lassi extends EventEmitter {
     log('startup component', component.name)
     const lassiInstance = this
     component.dependencies = this.defaultDependencies.concat(component.dependencies)
-    flow()
-    // Configuration des composants
-      .seq(function () {
-        component.configure()
-        this()
-      })
-    // Configuration des services
-      .seq(function () {
+    flow().seq(function () {
+      // Configuration des composants
+      component.configure()
+      this()
+    }).seq(function () {
+      // Configuration des services
+
       /**
        * résoud un service à l'ajoute à setupables et postSetupable si besoin
        * @private
        * @param {string} name
        */
-        function addService (name) {
-          if (added.has(name)) return
-          added.add(name)
-          const service = lassiInstance.services.resolve(name) // Permet de concrétiser les services non encore injectés
-          if (!service) throw new Error(`Le service ${name} n'a pu être résolu (il ne retourne probablement rien)`)
-          if (service.setup) setupables.push(service)
-          if (service.postSetup) postSetupable.push(service)
-        }
-        // pour mémoriser les services déjà ajoutés
-        const added = new Set()
-        // liste des setup à lancer
-        const setupables = []
-        // postSetup est utilisé pour les services qui ont des actions à faire quand tous les services
-        // sont dispo (setup terminé, par ex pour que la BDD soit initialisée)
-        // mais avant $server.start()
-        const postSetupable = []
-        const services = lassiInstance.services.services()
+      function addService (name) {
+        if (added.has(name)) return
+        added.add(name)
+        const service = lassiInstance.services.resolve(name) // Permet de concrétiser les services non encore injectés
+        if (!service) throw new Error(`Le service ${name} n'a pu être résolu (il ne retourne probablement rien)`)
+        if (service.setup) setupables.push(service)
+        if (service.postSetup) postSetupable.push(service)
+      }
+
+      // pour mémoriser les services déjà ajoutés
+      const added = new Set()
+      // liste des setup à lancer
+      const setupables = []
+      // postSetup est utilisé pour les services qui ont des actions à faire quand tous les services
+      // sont dispo (setup terminé, par ex pour que la BDD soit initialisée)
+      // mais avant $server.start()
+      const postSetupable = []
+      const services = lassiInstance.services.services()
       // on veut $settings puis $cache puis $entities dispos dans cet ordre,
       // pour que les autres setup puissent les utiliser
       ;['$settings', '$cache', '$entities'].forEach(addService)
-        Object.keys(services).forEach(addService)
-        // fin init
-        log('starting setup chain', Array.from(added).join(', '))
-        flow(setupables)
-          .seqEach(function (service) {
-            service.setup(this)
-          })
-          .seq(function () {
-            log('starting postSetup chain')
-            this(null, postSetupable)
-          })
-          .seqEach(function (service) {
-            log('postSetup', service.serviceName)
-            service.postSetup(this)
-          })
-          .done(this)
-      })
-      .seq(function () {
-        log('startup event')
-        lassiInstance.emit('startup')
-        cb()
-      })
-      .catch(cb)
+      Object.keys(services).forEach(addService)
+      // fin init
+
+      log('starting setup chain', Array.from(added).join(', '))
+      flow(setupables).seqEach(function (service) {
+        service.setup(this)
+      }).seq(function () {
+        log('starting postSetup chain')
+        this(null, postSetupable)
+      }).seqEach(function (service) {
+        log('postSetup', service.serviceName)
+        service.postSetup(this)
+      }).done(this)
+
+      // fin des setup, tout est fini on peut lancer startup
+    }).seq(function () {
+      log('startup event')
+      lassiInstance.emit('startup')
+      cb()
+    }).catch(cb)
   }
 
   /**
